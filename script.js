e1c552b8c66f78c5b40abac462ab5c2cb94523b5
Leonardo Osugi <leoosugi96@gmail.com>
2022-08-21 19:00:55 -0300

    algo esta mudando

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+const lista = [
+    "./img/bobrossparrot.gif",
+    "./img/explodyparrot.gif",
+    "./img/fiestaparrot.gif", 
+    "./img/metalparrot.gif",
+    "./img/revertitparrot.gif",
+    "./img/tripletsparrot.gif",
+    "unicornparrot.gif"
+];
+console.log(lista);
+const listaVazia = [];
+const listaVazia2 = [];
+
+const cards = document.querySelector('.card');
+
 let opcao = 0;
 
 while(opcao % 2 !== 0 || opcao < 4 || opcao > 14){
     opcao = Number(prompt("Com quantas cartas quer jogar[4 até 14 cartas, evite digitar numeros impares: ]"));
 }
 
+const aleatorio = lista.sort(comparador);
 
-const lista = document.querySelector("ul");
-const globalCards = lista.childNodes;
 
-globalCards.sort(comparador);
-console.log(globalCards);
+/*for(const i = 0; i < opcao; i++){
+    listaVazia.push(lista[i]);
+    listaVazia2.push(lista[i]);
+}*/
+
+listaVazia.concat(listaVazia2);
+console.log(listaVazia);
+
 
 function comparador() {
     return Math.random() - 0.5;
@@ -17,5 +37,5 @@ function comparador() {
 
 
 function virado(seletor) {
-    
+    seletor.classList.add('apareca');
 }
