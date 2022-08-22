aa3d08df66bbf73a94ba855426fc9e839da1356e
Leonardo Osugi <leoosugi96@gmail.com>
2022-08-22 03:16:20 -0300

    Almost There

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const lista = [
-    "./img/bobrossparrot.gif",
-    "./img/explodyparrot.gif",
-    "./img/fiestaparrot.gif", 
-    "./img/metalparrot.gif",
-    "./img/revertitparrot.gif",
-    "./img/tripletsparrot.gif",
-    "unicornparrot.gif"
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot", 
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
 ];
-console.log(lista);
-const listaVazia = [];
-const listaVazia2 = [];
+
+const cartas = [];
 
 const cards = document.querySelector('.card');
 
@@ -19,23 +18,75 @@ while(opcao % 2 !== 0 || opcao < 4 || opcao > 14){
     opcao = Number(prompt("Com quantas cartas quer jogar[4 até 14 cartas, evite digitar numeros impares: ]"));
 }
 
-const aleatorio = lista.sort(comparador);
+const alguma = document.querySelector('.all-cards');
+
+for( i = 0; i< (opcao/2); i++){
+    cartas[i] = `
+    <li class="card" data-git="${lista[i]}" onclick="virado(this)">
+        <img class="front-face" src="./img/front 13.svg" alt="">
+        <img class="back-face" src="./img/${lista[i]}.gif" alt=""> 
+    </li>
+    `;
+
+    
+
+    cartas[i+opcao/2]  = `
+    <li class="card" data-git="${lista[i]}" onclick="virado(this)">
+        <img class="front-face" src="./img/front 13.svg" alt="">
+        <img class="back-face" src="./img/${lista[i]}.gif" alt=""> 
+    </li>
+    `;
 
 
-/*for(const i = 0; i < opcao; i++){
-    listaVazia.push(lista[i]);
-    listaVazia2.push(lista[i]);
-}*/
 
-listaVazia.concat(listaVazia2);
-console.log(listaVazia);
+}
 
-
-function comparador() {
+function baralhador() {
     return Math.random() - 0.5;
-  }
+}
 
+cartas.sort(baralhador);
+
+for(i = 0; i< opcao; i++){
+    alguma.innerHTML += cartas[i];
+}
+
+
+let checkao = false;
+let firstCard = "";
+let secondCard = "";
 
 function virado(seletor) {
+    if(seletor === firstCard) return;
+
     seletor.classList.add('apareca');
+
+    if(!checkao){
+        checkao = true;
+        firstCard = seletor;
+        return;
+    }else{
+        secondCard = seletor;
+        checkao = false;
+        comparador();
+        return;
+    }
+}
+
+
+let cont = 0;
+function comparador(){
+    if(firstCard.dataset.git === secondCard.dataset.git){
+        firstCard.setAttribute("onclick", "");
+        secondCard.setAttribute("onclick","");
+    }else{
+        setTimeout(viradaReversa, 1000);
+    }
+    firstCard = "";
+    secondCard = "";
+}
+
+function viradaReversa(){
+    firstCard.classList.remove('apareca');
+    secondCard.classList.remove('apareca');
 }
